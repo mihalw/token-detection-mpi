75a8951baa087ce45bdbb076217f6b221ceb6dff
mihalw <34897327+mihalw@users.noreply.github.com>
2020-01-05 04:25:25 +0100

    Update kod.c

--- kod.c
+++ kod.c
@@ -4,10 +4,10 @@
 #define MSG_TAG 100
 #define MSG_SIZE 1
 
-bool resend = false, can_send_token = true;
+bool resend = false, can_send_token = false;
 int color = -1; // -1 to bialy, 1 to czarny
 int last_received_token = 1, last_sended_token = 1;
-int my_rank, sender, size, confirmation;
+int my_rank, size, confirmation;
 
 void SendToken(int sender, int receiver, int color)
 {
@@ -35,26 +35,23 @@ int main(int argc, char **argv)
 
 	if (my_rank == 0)
 	{
-		printf("%d: Chce wyslac token o kolorze %d do %d\n", my_rank, color, my_rank + 1);
+		can_send_token = true;
 		SendToken(my_rank, (my_rank + 1) % size, color);
-		MPI_Recv(&color, MSG_SIZE, MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
-		printf("%d: Otrzymalem token o wartosci %d od %d\n", my_rank, color, status.MPI_SOURCE);
-		sender = my_rank;
 	}
-	else
+
+	for (int i = 0; i < 3; i++)
 	{
-		can_send_token = false;
-		//printf("%d: Wysylam token o kolorze %d do %d\n", rank, color, rank + 1);
 		MPI_Recv(&color, MSG_SIZE, MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
-		//MPI_Send(&confirmation, MSG_SIZE, MPI_INT, my_rank-1, MSG_TAG, MPI_COMM_WORLD);
 		printf("%d: Otrzymalem token o wartosci %d od %d\n", my_rank, color, status.MPI_SOURCE);
-		printf("%d: Chce wyslac token o kolorze %d do %d\n", my_rank, color, (my_rank + 1) % size);
+		//MPI_Send(&confirmation, MSG_SIZE, MPI_INT, my_rank-1, MSG_TAG, MPI_COMM_WORLD);
+
 		if (color != last_received_token)
 		{
 			resend = false;
 			last_received_token = color;
 			can_send_token = true;
 		}
+		//MPI_Recv(&confirmation, MSG_SIZE, MPI_INT, my_rank+1, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
 		SendToken(my_rank, (my_rank + 1) % size, color);
 	}
 	MPI_Finalize();
