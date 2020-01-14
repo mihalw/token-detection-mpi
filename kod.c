1682edfc703a1fec49dcae0d48f0517059b6cf98
mihalw <34897327+mihalw@users.noreply.github.com>
2020-01-14 13:17:51 +0100

    Update kod.c

--- kod.c
+++ kod.c
@@ -8,43 +8,27 @@
 #define MSG_TOKEN 100
 #define MSG_CONF 5
 #define MSG_SIZE 1
-#define PROBABILITY_TOKEN 3
+#define SUCCESS_THRESHOLD 4
 #define PROBABILITY_CONF 2
-#define TIME_IN_CRITICAL_SECTION 15
+#define TIME_IN_CRITICAL_SECTION 10
 
-void my_token_send(int rank, int color_to_send, int receiver, int msg_name, MPI_Request req, int *last_sended_color)
+void my_send(int my_rank, int what_to_send, int receiver, int msg_name, MPI_Request req, int *last_sended_color, int color_received)
 {
-	bool sent = false;
-	while (!sent)
+	if (msg_name == MSG_TOKEN)
 	{
-		if ((rand() % 10) > PROBABILITY_TOKEN)
+		printf("%d: Wyslalem token w kolorze %d do procesu %d\n", my_rank, what_to_send, receiver);
+		if ((rand() % 10) > SUCCESS_THRESHOLD)
 		{
-			MPI_Isend(&color_to_send, MSG_SIZE, MPI_INT, receiver, msg_name, MPI_COMM_WORLD, &req);
-			printf("%d: Wyslalem token w kolorze %d do procesu %d\n", rank, color_to_send, receiver);
-			*last_sended_color = color_to_send;
-			sent = true;
-		}
-		else
-		{
-			printf("%d: Nieudany send tokenu!\n", rank);
+			MPI_Isend(&what_to_send, MSG_SIZE, MPI_INT, receiver, msg_name, MPI_COMM_WORLD, &req);
+			*last_sended_color = what_to_send;
 		}
 	}
-}
-
-void my_conf_send(int confirmation, int receiver, int msg_name, MPI_Request req, int my_rank, int color_received)
-{
-	bool sent = false;
-	while (!sent)
+	else if (msg_name == MSG_CONF)
 	{
-		if ((rand() % 10) > PROBABILITY_CONF)
+		printf("%d: Wyslalem potwierdzenie otrzymania tokenu w kolorze %d do procesu %d\n", my_rank, color_received, receiver);
+		if ((rand() % 10) > SUCCESS_THRESHOLD)
 		{
-			MPI_Isend(&confirmation, MSG_SIZE, MPI_INT, receiver, msg_name, MPI_COMM_WORLD, &req);
-			printf("%d: Wyslalem potwierdzenie otrzymania tokenu w kolorze %d do procesu %d\n", my_rank, color_received, receiver);
-			sent = true;
-		}
-		else
-		{
-			printf("%d: Nieudany send potwierdzenia!\n", my_rank);
+			MPI_Isend(&what_to_send, MSG_SIZE, MPI_INT, receiver, msg_name, MPI_COMM_WORLD, &req);
 		}
 	}
 }
@@ -58,7 +42,7 @@ int main(int argc, char **argv)
 	int flag_received = 0, flag_conf_recv = 0;
 	MPI_Status status_received, status_send, status_conf_recv, status_conf_send;
 	MPI_Request req_send, req_conf, req_rcv, req_ack;
-	time_t receive_token_time, leave_critical_section_time = 0; 
+	time_t receive_token_time, leave_critical_section_time = 0;
 
 	MPI_Init(&argc, &argv);
 	MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
@@ -71,10 +55,10 @@ int main(int argc, char **argv)
 
 	while (1)
 	{
-		sleep(rand() % 3 + 1); 
+		sleep(rand() % 3 + 1);
 		if (can_send_token && time(NULL) > leave_critical_section_time)
 		{
-			my_token_send(my_rank, color_to_send, (my_rank + 1) % size, MSG_TOKEN, req_send, &last_sended_color);
+			my_send(my_rank, color_to_send, (my_rank + 1) % size, MSG_TOKEN, req_send, &last_sended_color, 0);
 
 			if (!conf_wait_started)
 			{
@@ -86,7 +70,7 @@ int main(int argc, char **argv)
 
 			if (flag_conf_recv)
 			{
-				printf("%d: Potwierdzenie odebrania tokenu przez nastepny proces, przestaje go juz wysylac\n", my_rank);
+				printf("%d: Potwierdzenie odebrania tokenu przez proces %d, przestaje go juz wysylac\n", my_rank, status_conf_send.MPI_SOURCE);
 
 				can_send_token = false;
 				flag_conf_recv = false;
@@ -114,9 +98,8 @@ int main(int argc, char **argv)
 				printf("%d: Otrzymalem token w kolorze %d od procesu %d\n", my_rank, color_received, status_received.MPI_SOURCE);
 				can_send_token = true;
 
-				my_conf_send(confirmation, (my_rank + (size - 1)) % size, MSG_CONF, req_ack, my_rank, color_received);
-				//printf("%d: WCHODZE DO SEKCJI KRYTYCZNEJ!\n", my_rank);
-    			receive_token_time = time(NULL); 
+				my_send(my_rank, confirmation, (my_rank + (size - 1)) % size, MSG_CONF, req_ack, 0, color_received);
+				receive_token_time = time(NULL);
 				leave_critical_section_time = receive_token_time + TIME_IN_CRITICAL_SECTION;
 			}
 			flag_received = false;
